617696abfd95f420cb5abfcad189c0d82b16cb38
Yakir Perlin <yakirperlin@gmail.com>
2024-11-04 22:44:00 +0200

    update

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 import os
 from fastapi import FastAPI, Request, Depends
 from starlette.responses import JSONResponse
-import jwt
 from dotenv import load_dotenv
+import jwt
 
 load_dotenv()
 
 app = FastAPI()
 
-key = os.getenv("PUBLIC_KEY")
+public_key = os.getenv("PUBLIC_KEY")
 
 @app.middleware("http")
 async def check_token(request: Request, call_next):
     if request.headers.get("Authorization"):
         token = request.headers["Authorization"].split(" ")[1]
         try:
-            decoded = jwt.decode(token, key, algorithms=["RS256"])
-            request.state.decoded_token = decoded
-        except jwt.ExpiredSignatureError:
-            return JSONResponse(content={"error": "token expired"}, status_code=401)
-        except jwt.InvalidTokenError:
-            return JSONResponse(content={"error": "invalid token"}, status_code=401)
+            header_data = jwt.get_unverified_header(token)
+            payload_data = jwt.decode(jwt=token, key=public_key, algorithms=[header_data['alg'], ])
+            request.state.decoded_token = payload_data
+        except Exception as e:
+            return JSONResponse(content={"error": "invalid token", "details": str(e)}, status_code=401)
+
     else:
         return JSONResponse(content={"error": "unauthorized"}, status_code=401)
 
